c7dab756c1fe4e05892cfadca084ff3f6c52422f
VidadiMurad <vidad0murad@gmail.com>
2026-05-18 13:49:57 +0400

    Task 1 Finish

--- Orient.9.Tasks/Orient.9.Tasks/main.swift
+++ Orient.9.Tasks/Orient.9.Tasks/main.swift
@@ -3,9 +3,22 @@
 //  Orient.9.Tasks
 //
 //  Created by Vidadi Muradzada on 18.05.26.
-//
+//  Function Tasks
 
 import Foundation
 
-print("Hello, World!")
+
+//------------------------------------------------Task 1
+/*
+func doubleString(first:String, second:String) {
+    print("\(first) \(second)")
+}
+
+print("---------------------------------------")
+doubleString(first:"Vidadi", second:"Muradzada")
+print("---------------------------------------")
+ */
+//------------------------------------------------Task 1
+
+//------------------------------------------------Task 2
 
